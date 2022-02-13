da697a55fa79c4d515a33f7ae2ee42e574419c3b
AWils115 <wilsonalex23@googlemail.com>
2022-02-13 15:38:15 +0000

    fetch data from the API

--- src/App.js
+++ src/App.js
@@ -13,21 +13,46 @@ import Filter from './components/Filter/Filter';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  return (
-    <div className='App'>
-      <h1 className='text-center mb-3'>Characters</h1>
-      <div className='container'>
-        <div className='row'>
-          {/* Filter Component */}
-          <div className='col-lg-8 col-12'>
-            <div className='row'>
-              {/* Card Component */}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	/* API for Rick and Morty character data */
+	/**
+	 * * The useState() hook stores the data in the fetchedData variable and
+	 * * gives us a function key to change the varibale data whenever the useEffect()
+	 * * hook fetches new data. The fetchedData variable stores the data we get
+	 * * from the API, the updateFetchedData changes the data whenever we want.
+	 * * We destructure the data in fetchedData into info and results to make it
+	 * * easier later.
+	 */
+	let [fetchedData, updateFetchedData] = useState([]);
+	let { info, results } = fetchedData;
+
+	/**
+	 * * The useEffect() hook fetches the data from the rick and morty api,
+	 * * with a watch put on the api, so if the api variable changes it will
+	 * * load fresh data.
+	 * * The async function will fetch the raw data and convert it to JSON.
+	 */
+	let api = `https://rickandmortyapi.com/api/character/?page=1`;
+	useEffect(() => {
+		(async function () {
+			let data = await fetch(api).then((response) => response.json());
+			updateFetchedData(data);
+		})();
+	}, [api]);
+
+  /* Render the app */
+	return (
+		<div className='App'>
+			<h1 className='text-center mb-3'>Characters</h1>
+			<div className='container'>
+				<div className='row'>
+					{/* Filter Component */}
+					<div className='col-lg-8 col-12'>
+						<div className='row'>{/* Card Component */}</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
