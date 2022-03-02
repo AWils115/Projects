be70e071a1a9a1d9e43d793abfd02d8c38a6a97f
AWils115 <wilsonalex23@googlemail.com>
2022-03-02 19:07:07 +0000

    Initial navbar styling

--- garmin/src/App.js
+++ garmin/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
-      Hello React App!
+      <Navbar />
     </div>
   )
 }
---
+++ garmin/src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<div>
+			<nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+				<div className='container-fluid'>
+					<span className='navbar-brand mb-0 h1'>Fitness Dashboard</span>
+					<button
+						className='navbar-toggler'
+						type='button'
+						data-bs-toggle='collapse'
+						data-bs-target='#navbarNav'
+						aria-controls='navabrNav'
+						aria-expanded='false'
+						aria-label='Toggle navigation'
+					>
+						<span className='navbar-toggler-icon'></span>
+					</button>
+					<div className='collapse navbar-collapse' id='navbarNav'>
+						<ul className='navbar-nav'>
+							<li className='nav-item'>
+								<a href='#' className='nav-link active' aria-current='page'>
+									Home
+								</a>
+							</li>
+							<li className='nav-item'>
+								<a href='#' className='nav-link'>
+									Training
+								</a>
+							</li>
+							<li className='nav-item'>
+								<a href='#' className='nav-link'>
+									Nutrition
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	);
+};
+
+export default Navbar;
